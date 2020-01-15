43afcba322e09379fa8af08cc13ac9e1afafb61a
paru.upao <terahaya7991@gmail.com>
2020-01-15 09:35:49 +0000

    login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    
+    include SessionsHelper
+    
+    private
+    
+    def require_user_logged_in?
+        unless logged_in?
+            redirect_to login_url
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  
   root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users
+  
 end
